5469490168b87f320f9ec6869ff36e62af04131b
Harishankar S A <103807078+hari1050@users.noreply.github.com>
2024-11-09 13:06:56 +0530

    Delete supabaseConfig.js

--- supabaseConfig.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ccvfzxopmskzeegxucms.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjdmZ6eG9wbXNremVlZ3h1Y21zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NzEwNTgsImV4cCI6MjAyMzA0NzA1OH0.FmvWCFYbXEvSLGdNDQSoR-ioczkl7fKRiDSqgU-N-Rg';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
